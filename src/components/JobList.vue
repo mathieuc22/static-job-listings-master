f27cdac64dd473e8807bc21ff2f60c2c90c14b32
Mathieu <mathieu.collet@gmail.com>
2022-07-26 17:29:23 +0200

    Job list main component

---
+++ src/components/JobList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <li class="job" v-for="job in jobs" :key="job.id">
+            <img :src="job.logo" alt="">
+            {{ job.company }}
+            {{ job.position }}
+            {{ job.postedAt }}
+            {{ job.contract }}
+            {{ job.location }}
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import json from '../assets/data.json'
+
+const jobs = ref(json)
+
+</script>
+
+<style lang="scss" scoped>
+.job {
+    background: $color-neutral-White;
+}
+</style>
