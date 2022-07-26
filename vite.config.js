e221a1d906620ed6664bdc1a8badf38fa4215a10
Mathieu <mathieu.collet@gmail.com>
2022-07-26 17:08:15 +0200

    Base style and clean assets

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/static-job-listings-master/',
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "./src/assets/style.scss";
-        `
-      }
-    }
-  }
+  plugins: [vue()]
 })
