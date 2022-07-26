4a9086b4c810baeaea9e437f2e08c6ae98f4436e
Mathieu <mathieu.collet@gmail.com>
2022-07-26 17:30:05 +0200

    Change style.scss reference

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/static-job-listings-master/',
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/style.scss";
+        `
+      }
+    }
+  }
 })
