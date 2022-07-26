5496b84c9a155fdf8f7482d82aa047e30a183974
Mathieu <mathieu.collet@gmail.com>
2022-07-26 16:41:57 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/static-job-listings-master/',
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/assets/style.scss";
+        `
+      }
+    }
+  }
+})
